b794ba0d363d9719a54eac43b214f9423ec8eb74
deokgeneral <alsejrrl@naver.com>
2024-03-04 18:46:24 +0900

    1강 과제

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const http = reqire('http');
-const port = 4000;
-const server = http.createSever((req,res) => {
-    res.writeHead(200, {
-        'Content-Type' : 'text/plain'
-    });
-    res.end('Hello, world');
+const http = require('http');
+const PORT = 4000;
+const targetObject = { a: "a", b: "b" };
+
+const server = http.createServer((req, res) => {
+
+    if(req.method === 'POST' && req.url === '/home'){
+        req.on('data', (data) => {
+            console.log(data);
+            const stringfiedData = data.toString();
+            console.log(stringfiedData);
+            Object.assign(targetObject,JOSON.parse(stringfiedData))
+        })
+    } else {
+        if(req.url ==='/home'){
+            res.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            res.end(JSON.stringify(targetObject));
+            }else if(req.url ==='/about'){
+                res.setHeader('Content-Type', 'text/html');
+                res.write('<html>');
+                res.write('<body>');
+                res.write('<h1>About Page</h1>');
+                res.write('</body>');
+                res.write('</html>');
+            }else {
+                res.statusCode = 404;
+                res.end();
+            }
+        
+    }
 });
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-}); 
+});
